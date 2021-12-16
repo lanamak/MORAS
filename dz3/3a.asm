0691925df85c2d8b24aa17ed5c8d87f9fcdd33e6
lanamak <93083497+lanamak@users.noreply.github.com>
2021-12-16 10:28:36 +0100

    Add files via upload

---
+++ dz3/3a.asm
@@ -0,0 +1,82 @@
+//R0 baza 
+//R1 eksponent
+//spremi u R2 
+
+@R0
+D = M
+@baza
+M = D
+
+@R1
+D=M 
+@eksponent
+M=D
+
+@j
+M = 1   
+(PotenciranjeS)
+    @R1
+    D = M
+
+    @j
+    D = M - D    
+
+    @PotenciranjeE
+    D; JGE   // ako je D>=0 skoci
+
+    @LoopS
+    0; JMP   // bezuvjetno skoci na kraj 
+
+
+(LoopS)
+@i
+M = 0
+    (LoopStart)
+        @R0
+        D = M
+
+        @i
+        D = M - D   
+
+        @LoopE
+        D; JGE   //   D>= 0 
+
+
+        @baza 
+        D = M
+
+        @k
+        M = M + D   
+
+        @i
+        M = M + 1
+
+        @LoopStart
+        0; JMP
+
+    (LoopE)
+
+    @k
+    D = M
+
+    @baza
+    M = D
+
+    @k
+    M = 0
+
+    @j
+    M = M + 1
+
+    @PotenciranjeS
+    0; JMP
+
+(PotenciranjeE)
+@baza
+D = M
+@R2
+M = D
+
+(END)
+@END
+0; JMP
